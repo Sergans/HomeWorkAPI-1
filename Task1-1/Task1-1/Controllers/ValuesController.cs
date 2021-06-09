1c1a1799b6c8e62c44f43933c4182f097ec78414
Сергей <gan7er@yandex.ru>
2021-06-09 18:42:39 +0300

    Методы контроллера

--- Task1-1/Task1-1/Controllers/ValuesController.cs
+++ Task1-1/Task1-1/Controllers/ValuesController.cs
@@ -11,5 +11,53 @@ namespace Task1_1.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly Temp _temp;
+        public ValuesController(Temp temp)
+        {
+
+            _temp = temp;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            return Ok(_temp.tmp);
+        }
+        [HttpPost]
+        public IActionResult Create([FromQuery] int input, [FromQuery] int year, [FromQuery] int month, [FromQuery] int day)
+        {
+            DateTime date = new DateTime(year, month, day);
+            var item = new Temp() { TemperatureC = input, Date = date };
+            _temp.tmp.Add(item);
+
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Update([FromQuery] int year, [FromQuery] int month, [FromQuery] int day, [FromQuery] int input)
+        {
+            DateTime date = new DateTime(year, month, day);
+            foreach (Temp a in _temp.tmp)
+            {
+                if (date == a.Date)
+                {
+                    a.TemperatureC = input;
+                }
+            }
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Delete([FromQuery] int year, [FromQuery] int month, [FromQuery] int day)
+        {
+            DateTime date = new DateTime(year, month, day);
+            foreach (Temp a in _temp.tmp)
+            {
+                if (date == a.Date)
+                {
+                    _temp.tmp.Remove(a);
+                    return Ok();
+                }
+            }
+            return Ok();
+        }
     }
 }
---
+++ Task1-1/Task1-1/Temp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task1_1
+{
+    public class Temp
+    {
+
+        public int TemperatureC { get; set; }
+        public DateTime Date { get; set; }
+        public List<Temp> tmp = new List<Temp>();
+
+        public Temp()
+        {
+
+
+        }
+    }
+}
